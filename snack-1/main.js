05e99c41e3928f6bf3e6ce26e4d4d82fae54b691
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-08 14:58:52 +0100

    snack 1

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Creation of an array with the names of the guests
+const guestsList = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+// Creation of the second array
+const placeCards = guestsList.map((element, index) => {
+    // This array transforms the first one in an objects array and adds the remaining attributes
+    const guest = {
+        table: 'Tavolo Vip',
+        name: element,
+        place: index + 1,
+    }
+
+    return guest;
+});
+
+console.log(placeCards);
