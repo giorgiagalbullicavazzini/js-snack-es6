d8956cd16bdb571300c4d44fc648dfce82ab642f
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-08 15:54:15 +0100

    higher grades list and fix uppercase list

--- snack-2/main.js
+++ snack-2/main.js
@@ -31,20 +31,29 @@ const francescaDaPolenta = createStudent(120, 'Francesca da Polenta', 84);
 
 // Creation of an objects array containing the list of the students
 const studentList = [marcoDellaRovere, paolaCortellessa, andreaMantegna, gaiaBorromini, luigiGrimaldello, pieroDellaFrancesca, francescaDaPolenta];
+console.log(studentList);
 
 // Creation of a list containing the uppercased names of the students
+const studentTags = [];
 studentList.forEach((element) => {
     const studentName = element.name.toUpperCase();
-    console.log(studentName);
+    studentTags.push(studentName);
 });
 
+console.log(studentTags);
+
+// Creation of a list containing the names of the students with a grade value greater than 70
+const studentHigherGrades = studentList.filter((element) => {
+    if (element.grades > 70) {
+        return true;
+    }
+
+    return false;
+});
+
+console.log(studentHigherGrades);
 
 
-
-// The second list we need to create contains the names of the students with a grade value greater than 70
-// Using a `filter` loop, we can create this array
-// Starting from our very first list, we can set the condition to add to the current array only the students with the proper grade
-// IF the value is greater than 70, the condition is true and the element gets pushed in the current array
 // The third list contains the names of the students with a grade value greater than 70 and an id greater than 120
 // In order to create this last list, we can use another `filter` loop, almost equal to the previous one
 // The only difference is the condition given
