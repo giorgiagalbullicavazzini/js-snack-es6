cd4942e55ecd1eb482885d83bbbd7a69823a081b
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-08 16:17:56 +0100

    snack 2 into dom

--- snack-2/main.js
+++ snack-2/main.js
@@ -15,6 +15,9 @@ const createStudent = (studentId, fullName, studentGrades) => {
     return student;
   }
 
+// A function to create an element
+const elementCreation = (elementType) => document.createElement(elementType);
+
 
 ////////////
 // Main
@@ -33,11 +36,17 @@ const francescaDaPolenta = createStudent(120, 'Francesca da Polenta', 84);
 const studentList = [marcoDellaRovere, paolaCortellessa, andreaMantegna, gaiaBorromini, luigiGrimaldello, pieroDellaFrancesca, francescaDaPolenta];
 console.log(studentList);
 
+const containerUppercase = document.querySelector('.container.uppercase');
+
 // Creation of a list containing the uppercased names of the students
 const studentTags = [];
 studentList.forEach((element) => {
     const studentName = element.name.toUpperCase();
     studentTags.push(studentName);
+
+    const uppercaseList = elementCreation('div');
+    uppercaseList.innerHTML = studentName;
+    containerUppercase.append(uppercaseList);
 });
 
 console.log(studentTags);
@@ -53,6 +62,13 @@ const studentHigherGrades = studentList.filter((element) => {
 
 console.log(studentHigherGrades);
 
+const containerGrades = document.querySelector('.container.grades');
+studentHigherGrades.forEach((element) => {
+    const gradesList = elementCreation('div');
+    gradesList.innerHTML = `ID: ${element.id}<br>Nome: ${element.name}<br>Somma dei voti: ${element.grades}`;
+    containerGrades.append(gradesList);
+});
+
 // Creation of a list containing the names of the students with a grade value greater than 70 and an id greater than 120
 const studentHigherGradesId = studentHigherGrades.filter((element) => {
     if (element.id > 120) {
@@ -63,3 +79,10 @@ const studentHigherGradesId = studentHigherGrades.filter((element) => {
 });
 
 console.log(studentHigherGradesId);
+
+const containerGradesId = document.querySelector('.container.grades-id');
+studentHigherGradesId.forEach((element) => {
+    const gradesIdList = elementCreation('div');
+    gradesIdList.innerHTML = `ID: ${element.id}<br>Nome: ${element.name}<br>Somma dei voti: ${element.grades}`;
+    containerGradesId.append(gradesIdList);
+});
