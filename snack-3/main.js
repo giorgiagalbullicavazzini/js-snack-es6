c47e701172faed424e363c9a7b0556670e2e95b4
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-09 20:47:56 +0100

    snack 3 style

--- snack-3/main.js
+++ snack-3/main.js
@@ -28,17 +28,23 @@ const gravelTriban520Sub = createBike('Racing Bike Gravel, Triban GRVL 520 SUBCO
 const vanRysel105 = createBike('Racing Bike Van Rysel, EDR AF 105', 8.8);
 const cicloProWheel520 = createBike('Racing Bike Cicloturismo, RC 520 105 PROWHEEL', 10.4);
 const vanRyselUltegra = createBike('Racing Bike Van Rysel, EDR CF ULTEGRA', 7.7);
-const ultegra = createBike('Racing Bike EDR AF ULTEGRA', 8.6);
+const ultegra = createBike('Racing Bike, EDR AF ULTEGRA', 8.6);
 
 
 const racingBikes = [gravelTriban100, gravelTriban120, gravelTriban520, gravelTriban500, cicloTriban52, gravelTriban520Sub, vanRysel105, cicloProWheel520, vanRyselUltegra, ultegra];
 
+// Array alphabetically ordered
+const sortBikes = racingBikes.sort((a, b) => a.name.localeCompare(b.name));
+
 // The purpose of this code is to console print the bike with the lesser weight using destructuring and a template literal
-let lesserWeight = racingBikes[0].weight;
-let lesserWeightName = racingBikes[0].name;
+let lesserWeight = sortBikes[0].weight;
+let lesserWeightName = sortBikes[0].name;
+
+const bikesTable = document.getElementById('bikes');
+const fragment = document.createDocumentFragment();
 
 // We can use the destructuring assignment into a forEach loop to save in a variable the values of the current index bike
-racingBikes.forEach((element, index) => {
+sortBikes.forEach((element, index) => {
     const {name, weight} = element;
 
     // Going on with the indexes, we can compare each index with the previous one and save into the variable the one with the lesser weight
@@ -46,7 +52,18 @@ racingBikes.forEach((element, index) => {
         lesserWeightName = name;
         lesserWeight = weight;
     }
+
+    const tr = document.createElement('tr');
+    tr.innerHTML += `<td>${name}</td>`;
+    tr.innerHTML += `<td>${weight}</td>`;
+    fragment.append(tr);
 });
 
-let lesserWeightBike = `La bicicletta con il peso minore è la ${lesserWeightName}, che pesa solo ${lesserWeight}kg!`;
+bikesTable.append(fragment);
+
+const container = document.querySelector('.container');
+
+let lesserWeightBike = `<div>La bicicletta con il peso minore è la ${lesserWeightName}, che pesa solo ${lesserWeight} kg!</div>`;
 console.log(lesserWeightBike);
+
+container.innerHTML += lesserWeightBike;
